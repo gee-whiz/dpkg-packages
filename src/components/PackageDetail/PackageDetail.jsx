c6c1e200a67a46a9edc762b4063c9a368db5b133
George kapoya <george.kapoya@smarp.com>
2020-02-02 22:53:16 +0200

    change class components to arrow functions

--- src/components/PackageDetail/PackageDetail.jsx
+++ src/components/PackageDetail/PackageDetail.jsx
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
 import classes from './PackageDetail.module.css';
 
-
-class PackageDetail extends Component {
-    render() {
-        return <div className={classes.PackageDetail}>
-            <h2>Description</h2>
-            <p>
-                python-setuptools , python2.3-setuptools , python2.4-setuptools
-                    Description: Package Discovery and Resource Access using pkg_resources
-                             The pkg_resources module provides an API for Python libraries to
-                             access their resource files, and for extensible applications and
-                             frameworks to automatically discover plugins.  It also provides
-                             runtime support for using C extensions that are inside zipfile-format
-                             eggs, support for merging packages that have separately-distributed
-                             modules or subpackages, and APIs for managing Python's current
-                             "working set" of active packages.
-                </p>
-        </div>
-    }
+const packageDetail = () => {
+    return <div className={classes.PackageDetail}>
+        <h2>Name</h2>
+        <p>libws-commons-util-java</p>
+        <h2>Description</h2>
+        <p>python-setuptools , python2.3-setuptools , python2.4-setuptools
+                Description: Package Discovery and Resource Access using pkg_resources
+                         The pkg_resources module provides an API for Python libraries to
+                         access their resource files, and for extensible applications and
+                         frameworks to automatically discover plugins.  It also provides
+                         runtime support for using C extensions that are inside zipfile-format
+                         eggs, support for merging packages that have separately-distributed
+                         modules or subpackages, and APIs for managing Python's current
+                         "working set" of active packages.</p>
+        <h2>Dependencies</h2>
+        <li>python</li>
+        <li>java</li>
+    </div>
 }
 
-export default PackageDetail;
+export default packageDetail;
--- src/components/Packages/Package.jsx
+++ src/components/Packages/Package.jsx
@@ -1,23 +1,21 @@
 import React, { Component } from 'react';
 import classes from './Package.module.css'
 
-class Package extends Component {
-    renderPackage = () => {
-        return <div className={classes.Package}>
-            <h2>Packages</h2>
-            <div className={classes.PackageLink}>libws-commons-util-java</div>
-            <div className={classes.PackageLink}>python-pkg-resources</div>
-            <div className={classes.PackageLink}>tcpd</div>
-            <div className={classes.PackageLink}>libbsf-java</div>
-            <div className={classes.PackageLink}>libaspectj-java</div>
-            <div className={classes.PackageLink}>libslf4j-java</div>
+const packageNames = () => {
+    return (
+        <div className={classes.Packages}>
+            <div className={classes.Package}>
+                <h2>Packages</h2>
+                <div className={classes.PackageLink}>libws-commons-util-java</div>
+                <div className={classes.PackageLink}>python-pkg-resources</div>
+                <div className={classes.PackageLink}>tcpd</div>
+                <div className={classes.PackageLink}>libbsf-java</div>
+                <div className={classes.PackageLink}>libaspectj-java</div>
+                <div className={classes.PackageLink}>libslf4j-java</div>
+            </div>
         </div>
-    }
-    render() {
-        return <div className={classes.Packages}>
-            {this.renderPackage()}
-        </div>
-    }
+    );
+
 }
 
-export default Package;
+export default packageNames;
